89bda37b5327192df8c96168cd1cf49cfd148a9e
JoaoSantos129 <Joao-Alexandre.CARVALHO-SANTOS@cpnv.ch>
2022-02-16 11:48:20 +0100

    Initial commit

---
+++ BatailleNavale/main.c
@@ -0,0 +1,13 @@
+/**
+ * Auteur:      João Carvalho Santos
+ * Date:        16.02.22
+ * Titre:       Bataille Navale 0.1
+ * Description: Jeu Bataille Navale avec 5 bateaux, le joueur doit découvrir et détruire les 5 bateaux avec le minimum de coups
+ */
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
