5161f2204c63afe2790d13b37ee6bbb2acc7008d
JoaoSantos129 <Joao-Alexandre.CARVALHO-SANTOS@cpnv.ch>
2022-02-17 22:27:35 +0100

    affichage grille

--- BatailleNavale/main.c
+++ BatailleNavale/main.c
@@ -9,6 +9,27 @@
 #include <windows.h>
 
 /**
+ * Changer la couleur de touts les prochains caractères en bleu clair
+ */
+void cyanColor() {
+    printf("\033[0;36m");
+}
+
+/**
+ * Changer la couleur de touts les prochains caractères en jaune
+ */
+void yellowColor() {
+        printf("\033[0;33m");
+}
+
+/**
+ * Remettre par défaut la couleur des caractères
+ */
+void resetColor() {
+    printf("\033[0m");
+}
+
+/**
  * Affichage du début du jeu
  */
 void start(){
@@ -46,7 +67,7 @@ void start(){
 
 /**
  * Affiche le menu et demande au joueur ce qu'il veut faire
- * @return Ce que le joueur veut faire
+ * @return Ce que le joueur veut faire: 0 = quitter, 1 = jouer, 2 = regles du jeu, 3 affichher les scores
  */
 char menu(){
     // Préparation des variables
@@ -56,35 +77,126 @@ char menu(){
     // Afficher le menu tant que le premier caractère taper n'est pas 0, 1, 2 ou 3
     do {
         // Présennter les options au joueur
-        printf("\n\nWelcome to Battleship!\n\n");
-        printf("Chose an option:\n");
-        printf(" 0 - Quit\n");
-        printf(" 1 - Play\n");
-        printf(" 2 - Rules\n");
-        printf(" 3 - Best Scores\n\n");
-        printf("Choice:");
+        cyanColor();
+        printf("\n\n  Welcome to Battleship!\n\n");
+        resetColor();
+        printf("  Chose an option:\n");
+        printf("    0 - Quit\n");
+        printf("    1 - Play\n");
+        printf("    2 - Rules\n");
+        printf("    3 - Best Scores\n\n");
+        printf("  Choice:");
         // Récupération du choix du joueur
         scanf("%c", &playerChoice);
 
         // Supprimer touts les caractères de la variable playerChoice sauf le premier
         while((characterEater = getchar()) != '\n' && characterEater != EOF);
+
         // Nettoyer l'interface
         system("cls");
+
+        if(playerChoice < '0' || playerChoice > '3'){
+            yellowColor();
+            printf("\n  PLEASE CHOSE AN AVAILABLE OPTION!\n\n");
+            resetColor();
+        }
     }while(playerChoice < '0' || playerChoice > '3');
     // Retourné le choix du joueur
     return playerChoice;
 }
 
+/**
+ * Affichage des règles du jeu
+ */
+void rules(){
+    printf("  In this game I have secretly placed 5 warships in a grid:\n");
+    printf("    - A Carrier, which occupies 5 spaces\n");
+    printf("    - A Battleship, which occupies 4 spaces\n");
+    printf("    - A Cruiser, which occupies 3 spaces\n");
+    printf("    - A Submarine, which occupies 3 spaces\n");
+    printf("    - A Destroyer, which occupies 2 spaces\n\n");
+    printf("  Your goal is to guess where they are by shooting at coordinates (a letter and a number).\n");
+    printf("  Boats will always be placed vertically or horizontally, but never diagonally!\n");
+    printf("  Once you shot on all the boat's different spaces, that boat is destroyed!\n");
+    printf("  Once all the boats are destroyed, you win!\n");
+    printf("  The less shots you use, the better your score is!\n\n");
+    printf("  Possible events:\n");
+    printf("    - ' ' = Nothing happened yet\n");
+    printf("    - 'O' = Miss\n");
+    printf("    - 'X' = Shot\n");
+    printf("    - '#' = Destroyed boat space\n\n\n");
+    system("pause");
+    system("cls");
+}
+
+/**
+ * Affiche la grille
+ */
+void grid(){
+    printf("\n\n    ╔═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╗\n"
+           "    ║     ║  A  ║  B  ║  C  ║  D  ║  E  ║  F  ║  G  ║  H  ║  I  ║  J  ║\n"
+           "    ╠═════╬═════╬═════╬═════╬═════╬═════╬═════╬═════╬═════╬═════╬═════╣\n");
+
+    for (int i = 1; i < 10; i++) {
+        printf("    ║  %d  ║     ║     ║     ║     ║     ║     ║     ║     ║     ║     ║\n"
+               "    ╠═════╬═════╬═════╬═════╬═════╬═════╬═════╬═════╬═════╬═════╬═════╣\n", i);
+    }
+
+    printf("    ║ 10  ║     ║     ║     ║     ║     ║     ║     ║     ║     ║     ║\n"
+           "    ╚═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╝\n\n");
+}
+
+/**
+ * Force le programme à se mettre en plein écran
+ */
+void fullscreen()
+{
+    keybd_event(VK_MENU,0x38,0,0);
+    keybd_event(VK_RETURN,0x1c,0,0);
+    keybd_event(VK_RETURN,0x1c,KEYEVENTF_KEYUP,0);
+    keybd_event(VK_MENU,0x38,KEYEVENTF_KEYUP,0);
+}
+
 int main() {
+    fullscreen();
+
     // Commande pour les accents et caractères spéciaux
     SetConsoleOutputCP(65001);
 
     // Préparation des variables
-    char choice = 0;
+    char choice;
 
     start();
 
-    choice = menu();
+    // Boucle infini. Tourner le jeu tant que choice != 3
+    for(;;) {
+        choice = menu();
 
-    return 0;
+        // Directionner le joueur chez la fonction choiosi
+        switch (choice) {
+            // Quitter le jeu
+            case '0':
+                return 0;
+
+            // Jouer
+            case '1':
+                grid();
+                system("pause");
+
+                break;
+
+            // Afficher les règles du jeu
+            case '2':
+                rules();
+
+                break;
+
+            // Afficher les scores
+            default:
+                printf("Pas encore disponible\n");
+                system("pause");
+
+                break;
+        }
+    }
 }
